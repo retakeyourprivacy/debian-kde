fee86370971a8db31ae1cde6d07ee54849e0ecec
Retake Your Privacy <contact@retakeyourprivacy.com>
2023-10-28 22:00:56 -0400

    fixed konsave call (during the install)
    while installing the profile, sourcing the 'aliasrc' file does not enable the newly added alias. therefore, that line has been removed, and the 'konsave' call uses the absolute path.
    
    in addition, the 'do-thru-ui.md' file was 'cat' twice because of a duplicate line in the 'konsave' module. it has also been removed.

--- modules/do-thru-ui.sh
+++ modules/do-thru-ui.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# stuff to do through UI until it's written as code
 cat do-thru-ui.md
--- modules/konsave-bismuth.sh
+++ modules/konsave-bismuth.sh
@@ -6,11 +6,7 @@ python3 -m venv ~/.local/bin/python3
 
 # use `sed` or `tee` to add `konsave` alias to the end of the aliasrc file
 echo -e '\nalias konsave="~/.local/bin/python3/bin/konsave"' >> ~/.config/shell/aliasrc
-source ~/.config/shell/aliasrc
 
 # use `konsave` to import and apply the tiling window manager setup
-konsave -i bismuth-tiling.knsv
-konsave -a bismuth-tiling
-
-# stuff to do through UI until it's written as code
-cat do-thru-ui.md
+~/.local/bin/python3/bin/konsave -i bismuth-tiling.knsv
+~/.local/bin/python3/bin/konsave -a bismuth-tiling
