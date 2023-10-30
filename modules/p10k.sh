3b4091bf482f7e32ae4da4a374caa34100f255bb
Retake Your Privacy <contact@retakeyourprivacy.com>
2023-10-29 23:19:48 -0400

    cleaned up shell.sh and p10k.sh

--- modules/p10k.sh
+++ modules/p10k.sh
@@ -1,37 +1,53 @@
 #!/bin/sh
 
+################################
+# FUNCTIONS
+################################
+
+installfonts() {
+    fonts=(
+    "MesloLGS_NF_Regular.ttf"
+    "MesloLGS_NF_Bold.ttf"
+    "MesloLGS_NF_Italic.ttf"
+    "MesloLGS_NF_Bold_Italic.ttf"
+    )
+
+    echo "There are $(echo ${#fonts[@]}) fonts to install. It shouldn't take long."
+    echo "Elevated privileges are required to properly install the fonts. Please enter your password if asked."
+
+    for font in "${fonts[@]}"; do
+        webfont=$(echo $font | sed 's/_/%20/g')
+        [ ! -e /usr/local/share/fonts/m/$font ] && echo "now installing $font ..." && sudo curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/$webfont -o /usr/local/share/fonts/m/$font > /dev/null 2>&1
+    done
+}
+
+packagedependencychecks() {
+    pkgs=(
+    "curl"
+    "git"
+    "zsh"
+    )
+
+    for pkg in "${pkgs[@]}"; do
+        [ ! -e /usr/bin/$pkg ] && echo "$pkg is not installed on this computer. Please install $pkg before proceeding." && needpkg="yes"
+    done
+
+    [[ $needpkg = "yes" ]] && exit 1
+}
+
+################################
+# ACTUAL SCRIPT
+################################
+
 # Package dependency checks
-pkgs=(
-"curl"
-"git"
-"zsh"
-)
-
-for pkg in "${pkgs[@]}"; do
-    [ ! -e /usr/bin/$pkg ] && echo "$pkg is not installed on this computer. Please install $pkg before proceeding." && needpkg="yes"
-done
-
-[[ $needpkg = "yes" ]] && exit 1
+packagedependencychecks
 
 # Directories
 [ ! -d /usr/local/share/fonts/m/ ] && sudo mkdir -p /usr/local/share/fonts/m/
 [ ! -d ~/.cache/zsh ] && mkdir -p ~/.cache/zsh
 
 # Install fonts
-fonts=(
-"MesloLGS_NF_Regular.ttf"
-"MesloLGS_NF_Bold.ttf"
-"MesloLGS_NF_Italic.ttf"
-"MesloLGS_NF_Bold_Italic.ttf"
-)
-
-echo "There are $(echo ${#fonts[@]}) fonts to install. It shouldn't take long."
-echo "Elevated privileges are required to properly install the fonts. Please enter your password if asked."
-
-for font in "${fonts[@]}"; do
-    webfont=$(echo $font | sed 's/_/%20/g')
-    [ ! -e /usr/local/share/fonts/m/$font ] && echo "now installing $font ..." && sudo curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/$webfont -o /usr/local/share/fonts/m/$font > /dev/null 2>&1
-done
+installfonts
 
 # Install 'powerlevel10k'
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/src/powerlevel10k > /dev/null 2>&1
--- modules/pkgs-kde.sh
+++ modules/pkgs-kde.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Packages for Debian-KDE using `kwin-bismuth`, a tiling window manager (like `dwm`):
-# NB: 'neovim' has been removed because the version on the Debian repos is too old
-
 while read pkg; do
     echo "$pkg is not installed on this computer. Installing $pkg now..."
     sudo nala install $pkg -y >/dev/null 2>&1
