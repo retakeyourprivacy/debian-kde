7afcf95301f0b7a0a0f417430b883016f2bef92c
Retake Your Privacy <contact@retakeyourprivacy.com>
2023-11-01 12:28:53 -0400

    updated the profiles again & upgraded flatpak script

--- modules/flatpaks.sh
+++ modules/flatpaks.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
+################################
+# FUNCTIONS
+################################
+
+paks=[
+"com.github.tchx84.Flatseal"
+"com.discordapp.Discord"
+"com.spotify.Client"
+"com.github.k4zmu2a.spacecadetpinball"
+]
+
+flatinstall() {
+    for pak in "${paks[@]}"; do
+        flatpak install flathub $pak -y
+    done
+}
+
+################################
+# ACTUAL SCRIPT
+################################
+
 # sets up flatpak to reference the flathub repos
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatinstall="flatpak install flathub"
-
-# flatpak for Flatseal, Discord, Spotify, and Windows XP Pinball
-$flatinstall com.github.tchx84.Flatseal
-$flatinstall com.discordapp.Discord
-$flatinstall com.spotify.Client
-$flatinstall com.github.k4zmu2a.spacecadetpinball
+flatinstall
